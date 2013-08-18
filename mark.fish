4ed56ec24a904a05b2757be8a14db98653d1c921
Tanner Smith <tanner@tssoftware.net>
2013-08-18 17:48:44 -0400

    Mark it so you can't create a mark with no name.

--- mark.fish
+++ mark.fish
@@ -1,3 +1,7 @@
 function mark
-  mkdir -p $MARKPATH; and ln -s (pwd) $MARKPATH/$argv[1]
+  if test $argv[1]
+    mkdir -p $MARKPATH; and ln -s (pwd) $MARKPATH/$argv[1]
+  else
+    echo "No mark specified."
+  end
 end
